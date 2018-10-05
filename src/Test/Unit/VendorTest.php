d6d3bbbdf957f4d5a4d1f1c6d4e593f58ed61e3b
Dalton Whyte <dalton.whyte@extended-content.com>
2018-10-05 21:09:03 +0100

    Added test cases for VendorTest suite.

--- src/Test/Unit/VendorTest.php
+++ src/Test/Unit/VendorTest.php
@@ -5,9 +5,41 @@
  * User: Dalton Whyte
  */
 use PHPUnit\Framework\TestCase;
-require __DIR__ . "../../src/Models/Vendor.php";
+require __DIR__ . "/../../Models/Vendor.php";
 
 class VendorTest extends TestCase
 {
+    public function testName_WithName_ReturnsName()
+    {
+        $vendor = new Vendor();
+        $vendor->setName("SubWay");
+        $this->assertEquals("SubWay", $vendor->getName());
+    }
 
+    public function testNull_WithName_ReturnsNull()
+    {
+        $vendor = new Vendor();
+        $vendor->setName(null);
+        $this->assertNull($vendor->getName());
+    }
+
+    public function testPostCode_With2PostCode_ReturnsPostCode(){
+        $vendor = new Vendor();
+        $vendor->setPostCode("NW352A");
+        $this->assertEquals("NW352A", $vendor->getPostCode());
+    }
+
+    public function testMaxHeadCount_WithMaxHeadCount_ReturnsMaxHeadCount()
+    {
+        $vendor = new Vendor();
+        $vendor->setMaxHeadCount("50");
+        $this->assertEquals("50", $vendor->getMaxHeadCount());
+    }
+
+    public function testNull_WithWithMaxHeadCount_ReturnsNull()
+    {
+        $vendor = new Vendor();
+        $vendor->setMaxHeadCount(null);
+        $this->assertNull($vendor->getMaxHeadCount());
+    }
 }
