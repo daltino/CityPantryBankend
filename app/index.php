c247b2353ebb3b596ceb1c032fd1f9ca4c9d5077
Dalton Whyte <dalton.whyte@extended-content.com>
2018-10-05 01:57:12 +0100

    Switched of error display for production.

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@
  * User: Dalton Whyte
  */
 error_reporting(E_ALL);
-ini_set('display_errors', TRUE); // Error display - OFF in production env or real server
+ini_set('display_errors', FALSE); // Error display - OFF in production env or real server
 ini_set('log_errors', TRUE); // Error logging
 ini_set('error_log', '../logs/errors.log'); // Logging file
 ini_set('log_errors_max_len', 1024); // Logging file size
